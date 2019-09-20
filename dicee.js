3a4c1c27b296b1907f5807955f914908206df125
So Wen Lee <53460340+eddieelee@users.noreply.github.com>
2019-09-20 12:53:47 -0700

    Add html, css, js and image files

---
+++ dicee.js
@@ -0,0 +1,24 @@
+// Dice 01
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1 ~ 6
+
+var randomDiceNumber1 = "dice" + randomNumber1 + ".png"; //dice1.png ~ dice6.png
+
+var randomImageSource1 = "images/" + randomDiceNumber1; //images/dice1.png ~ images/dices6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+// Dice 02
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// If
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "😀 Player 1 Wins ~!!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins ~!! 😄";
+} else {
+  document.querySelector("h1").innerHTML = " = = Draw = = ";
+}
